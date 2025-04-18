20bbb5985858c8687ea6701fc92524e170d76161
mariyancholakov <m.cholakov09@gmail.com>
2025-04-18 17:34:59 +0300

    Created basic structure of the frontend and installed tailwindcss and other packages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold">
+        Welcome to SmartReceipt
+      </h1>
+    </>
+  );
+}
+
+export default App;
