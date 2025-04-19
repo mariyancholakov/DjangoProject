5c546715d0f2544f96b79113816e9a668cb31688
mariyancholakov <m.cholakov09@gmail.com>
2025-04-19 16:44:24 +0300

    Created a serializer and created a view with get, post, put and delete routes

--- backend/receipts/views.py
+++ backend/receipts/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Receipt
+from .serializers import ReceiptSerializer
 
 # Create your views here.
+
+class ReceiptListCreateAPIView(APIView):
+    
+    def get(self, request):
+        receipts = Receipt.objects.all()
+        serializer = ReceiptSerializer(receipts, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ReceiptSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class ReceiptRetrieveUpdateDeleteAPIView(APIView):
+    def get(self, request, pk):
+        try:
+            receipt = Receipt.objects.get(pk=pk)
+        except Receipt.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ReceiptSerializer(receipt)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        try:
+            receipt = Receipt.objects.get(pk=pk)
+        except Receipt.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ReceiptSerializer(receipt, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            receipt = Receipt.objects.get(pk=pk)
+        except Receipt.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        receipt.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
--- backend/smartreceipt/settings.py
+++ backend/smartreceipt/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'receipts',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
